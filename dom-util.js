013393c266df7dfc13bfa8f083478e0778f5fd71
Joseph Lei <josephlei@Josephs-MacBook-Pro.local>
2017-05-13 12:05:51 -0700

    finish dom-util

--- dom-util.js
+++ dom-util.js
@@ -1,12 +1,14 @@
-
 const visitAllNodes = function(node, callback) {
   // Hint: read about DOM nodes and node methods here: https://developer.mozilla.org/en-US/docs/Web/API/Node
-  // Your code here
+  
+  node.childNodes.forEach(smallerNode => visitAllNodes(smallerNode, callback));
+  callback(node);
 };
 
 const flattenTreeToArray = function(node) {
-  // Hint: Use visitAllNodes()
-  // Your code here
+  let arr = [];
+  visitAllNodes(node, value => arr.push(value));
+  return arr;
 };
 
 module.exports = {
